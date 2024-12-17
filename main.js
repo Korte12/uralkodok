e555804d1fe27774e0d6787c88d216bafca39e7d
Roli <rolandkortvelyesi12@gmail.com>
2024-12-17 23:54:03 +0100

    linearis utasitas vegrehajtas

--- main.js
+++ main.js
@@ -1,23 +1,101 @@
-const table = document.createElement('table'); //Létrehozok egy table elemet ami a táblázatom lesz
-document.body.appendChild(table); //Hozzá appendelem a body-hoz
+const table = document.createElement('table'); //Létrehozom a table-t
+document.body.appendChild(table);//Hozzá appendelem a body-hoz
 
-const thead = document.createElement('thead'); //Létrehozok egy thead elemet
-table.appendChild(thead);//Hozzá appendelem a táblázathoz
+const thead = document.createElement('thead');//Létrehozom a thead-et
+table.appendChild(thead);//Hozzá appendelem a table-hez
 
-const tr = document.createElement('tr');//Létrehozok egy sor elemet
-thead.appendChild(tr);//Hozzá appendelem a fej részhez
+const tr = document.createElement('tr');//Létrehozom a tr-t
+thead.appendChild(tr);//Hozzá appendelem a thead-hez
 
-const tbody = document.createElement('tbody');//Létrehozok egy tbody elemet
-table.appendChild(tbody)//Hozzá appendelem a table-höz
+const th1 = document.createElement('th');//Létrehozom az első th-t
+th1.innerHTML = "Uralkodó"; //Megadom az értékét
+tr.appendChild(th1);//Hozzá appendelem a sorhoz
 
-const uralkodo_nev = document.createElement('th')//Létrehozok egy th elemet
-uralkodo_nev.innerHTML = "Uralkodo" //Megadom az értékét
-tr.appendChild(uralkodo_nev);//Hozzá appendelem a sorhoz
+const th2 = document.createElement('th');//Létrehozom a második th-t
+th2.innerHTML = "Esemény";//Megadom az értékét
+tr.appendChild(th2);//Hozzá appendelem a sorhoz
 
-const esemeny = document.createElement('th')//Létrehozok egy th elemet
-esemeny.innerHTML = "Esemeny"//Megadom az értékét
-tr.appendChild(esemeny)//Hozzá appendelem a sorhoz
+const th3 = document.createElement('th');//Létrehozom a harmadik th-t
+th3.innerHTML = "Évszám";//Megadom az értékét
+tr.appendChild(th3);//Hozzá appendelem a sorhoz
 
-const evszam = document.createElement('th');//Létrehozok egy th elemet
-evszam.innerHTML = "Evszam"//Megadom az értékét
-tr.appendChild(evszam)//Hozzá appendelem a sorhoz
+const tbody = document.createElement('tbody');//Létrehozom a tbody-t
+table.appendChild(tbody);//Hozzá appendelem a table-hez
+
+const row1 = document.createElement('tr');//Létrehozom az első sort
+tbody.appendChild(row1);// Hozzá appendelem a tbody-hoz
+const cell1_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell1_1.innerHTML = "I. István";//Megadom az értékét
+cell1_1.rowSpan = 2;//rowSpan-t állitok be hogy jó legyen az elrendezés
+row1.appendChild(cell1_1);//Hozzá appendelem a sorhoz a cellát
+const cell1_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell1_2.innerHTML = "Koronázás";//Megadom az értékét
+row1.appendChild(cell1_2);//Hozzá appendelem a sorhoz a cellát
+const cell1_3 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell1_3.innerHTML = "1000";//Megadom az értékét
+row1.appendChild(cell1_3);//Hozzá appendelem a sorhoz a cellát
+
+const row2 = document.createElement('tr');//Létrehozom a második sort
+tbody.appendChild(row2);// Hozzá appendelem a tbody-hoz
+const cell2_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell2_1.innerHTML = "Pannonhalmi apátság megalapítása";//Megadom az értékét
+row2.appendChild(cell2_1);//Hozzá appendelem a sorhoz a cellát
+const cell2_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell2_2.innerHTML = "1001";//Megadom az értékét
+row2.appendChild(cell2_2);//Hozzá appendelem a sorhoz a cellát
+
+const row3 = document.createElement('tr');//Létrehozom a harmadik sort
+tbody.appendChild(row3);// Hozzá appendelem a tbody-hoz
+const cell3_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell3_1.innerHTML = "IV. Béla";//Megadom az értékét
+row3.appendChild(cell3_1);//Hozzá appendelem a sorhoz a cellát
+const cell3_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell3_2.innerHTML = "Tatarjárás";//Megadom az értékét
+row3.appendChild(cell3_2);//Hozzá appendelem a sorhoz a cellát
+const cell3_3 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell3_3.innerHTML = "1241-1242";//Megadom az értékét
+row3.appendChild(cell3_3);//Hozzá appendelem a sorhoz a cellát
+
+const row4 = document.createElement('tr');//Létrehozom a negyedik sort
+tbody.appendChild(row4);// Hozzá appendelem a tbody-hoz
+const cell4_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell4_1.innerHTML = "Mátyás király";//Megadom az értékét
+row4.appendChild(cell4_1);//Hozzá appendelem a sorhoz a cellát
+cell4_1.rowSpan = 2;//rowSpan-t állitok be hogy jó legyen az elrendezés
+const cell4_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell4_2.innerHTML = "Bécs elfoglalása";//Megadom az értékét
+row4.appendChild(cell4_2);//Hozzá appendelem a sorhoz a cellát
+const cell4_3 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell4_3.innerHTML = "1485";//Megadom az értékét
+row4.appendChild(cell4_3);//Hozzá appendelem a sorhoz a cellát
+
+const row5 = document.createElement('tr');//Létrehozom az ötödik sort
+tbody.appendChild(row5);// Hozzá appendelem a tbody-hoz
+const cell5_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell5_1.innerHTML = "Kenyérmezei csata";//Megadom az értékét
+row5.appendChild(cell5_1);//Hozzá appendelem a sorhoz a cellát
+const cell5_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell5_2.innerHTML = "1479";//Megadom az értékét
+row5.appendChild(cell5_2);//Hozzá appendelem a sorhoz a cellát
+
+const row6 = document.createElement('tr');//Létrehozom hatodik sort
+tbody.appendChild(row6);// Hozzá appendelem a tbody-hoz
+const cell6_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell6_1.innerHTML = "II. Rákóczi Ferenc";//Megadom az értékét
+cell6_1.rowSpan = 2;//rowSpan-t állitok be hogy jó legyen az elrendezés
+row6.appendChild(cell6_1);//Hozzá appendelem a sorhoz a cellát
+const cell6_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell6_2.innerHTML = "A szabadságharc kezdete";//Megadom az értékét
+row6.appendChild(cell6_2);//Hozzá appendelem a sorhoz a cellát
+const cell6_3 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell6_3.innerHTML = "1703";//Megadom az értékét
+row6.appendChild(cell6_3);//Hozzá appendelem a sorhoz a cellát
+
+const row7 = document.createElement('tr');//Létrehozom a hetedik sort
+tbody.appendChild(row7);// Hozzá appendelem a tbody-hoz
+const cell7_1 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell7_1.innerHTML = "A szabadságharc vége";//Megadom az értékét
+row7.appendChild(cell7_1);//Hozzá appendelem a sorhoz a cellát
+const cell7_2 = document.createElement('td');//Létrehozok egy td-t a táblázat egyik celláját
+cell7_2.innerHTML = "1711";//Megadom az értékét
+row7.appendChild(cell7_2);//Hozzá appendelem a sorhoz a cellát
