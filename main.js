c9fe99a890060a50a7431772553044419bcf4e92
Roli <rolandkortvelyesi12@gmail.com>
2024-12-18 13:06:17 +0100

    esemenykezelo bevezetese

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const array = [// A tömb létehozása
     {// A tömb első elemét létrehozom
-        nev : "I. István",//Értékadás
+        uralkodo_nev : "I. István",//Értékadás
         esemeny1 : "Koronázás",//Értékadás
         esemeny2 : "Pannonhalmi apátság megalapítása",//Értékadás
         evszam1 : "1000",//Értékadás
         evszam2 : "1001"//Értékadás
     },
     {// A tömb második elemét létrehozom
-        nev : "IV. Béla",//Értékadás
+        uralkodo_nev : "IV. Béla",//Értékadás
         esemeny1 : "tatárjárás",//Értékadás
         evszam1 : "1241-1242"//Értékadás
     },
     {// A tömb harmadik elemét létrehozom
-        nev : "Mátyás király",//Értékadás
+        uralkodo_nev : "Mátyás király",//Értékadás
         esemeny1 : "Bécs elfoglalása",//Értékadás
         esemeny2 : "Kenyérmezei csata",//Értékadás
         evszam1 : "1485",//Értékadás
         evszam2 : "1479"//Értékadás
     },
     {// A tömb negyedik elemét létrehozom
-        nev : "II. Rákóczi Ferenc",//Értékadás
+        uralkodo_nev : "II. Rákóczi Ferenc",//Értékadás
         esemeny1 : "A szabadságharc kezdete",//Értékadás
         esemeny2 : "A szabadságharc vége",//Értékadás
         evszam1 : "1703",//Értékadás
@@ -54,6 +54,37 @@ tr.appendChild(evszam)//Hozzá appendelem a sorhoz
 
 render();//Meghivom a render függvényt
 
+const form = document.getElementById("form") //Lekérem a html form id-ját
+
+form.addEventListener('submit', function(e){//Eseménykezelőt adok a form-hoz
+    const uralkodo_nev = document.getElementById("uralkodo_nev")//Lekérem a html form id-ját
+    const esemeny1 = document.getElementById("esemeny1")//Lekérem a html form id-ját
+    const esemeny2 = document.getElementById("esemeny2")//Lekérem a html form id-ját
+    const evszam1 = document.getElementById("evszam1")//Lekérem a html form id-ját
+    const evszam2 = document.getElementById("evszam2")//Lekérem a html form id-ját
+
+    const uralkodo_nevvalue = uralkodo_nev.value ///Eltárolom egy változóban az értéket
+    const esemeny1value = esemeny1.value///Eltárolom egy változóban az értéket
+    const esemeny2value = esemeny2.value///Eltárolom egy változóban az értéket
+    const evszam1value = evszam1.value///Eltárolom egy változóban az értéket
+    const evszam2value = evszam2.value//Eltárolom egy változóban az értéket
+
+    e.preventDefault()//Megakadályozom hogy a böngésző alapártelmezetten lefusson
+
+    const new_person = { //Létrehozok egy új elemet
+        uralkodo_nev: uralkodo_nevvalue, //Értéket adok
+        esemeny1: esemeny1value,//Értéket adok
+        esemeny2: esemeny2value,//Értéket adok
+        evszam1: evszam1value,//Értéket adok
+        evszam2: evszam2value//Értéket adok
+    }
+
+    array.push(new_person)//Hozzárakom az arrayhez az új elemet
+    table.innerHTML = ''//Üres string-et használok törlődik a táblázat
+    render();//Meghivom a render függvényt mégegyszer
+
+})
+
 function render(){//Elkezdem megirni a render függvényt
     const tablebody = tbody;//Létrehozok egy tablebody-t aminek az értéke tbody
     tbody.innerHTML = ''; // tbody innerHtml-je üres string
@@ -62,7 +93,7 @@ function render(){//Elkezdem megirni a render függvényt
         let row = document.createElement('tr');//Létrehozok egy tr-t
 
         const uralkodoCell = document.createElement('td'); //Létrehozok egy td-t
-        uralkodoCell.innerHTML = element.nev; //Megadom az innerHTML értékét
+        uralkodoCell.innerHTML = element.uralkodo_nev; //Megadom az innerHTML értékét
         uralkodoCell.rowSpan = element.esemeny2 ? 2 : 1; // rowSpan-t vezetek be a megfelelő elrendezés érdekében
         row.appendChild(uralkodoCell);//Hozzá appendelem a sorhoz
 
