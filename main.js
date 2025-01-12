d7c928421d4fcb0b982cf37127e8056f68d4dd00
Roli <rolandkortvelyesi12@gmail.com>
2025-01-12 20:09:04 +0100

    tabla fejlec generalas bevezetese

--- main.js
+++ main.js
@@ -27,30 +27,32 @@ const array = [// A tömb létehozása
     }
 ]
 
+    const table = document.createElement('table'); //Létrehozok egy table elemet ami a táblázatom lesz
+    document.body.appendChild(table); //Hozzá appendelem a body-hoz
 
-const table = document.createElement('table'); //Létrehozok egy table elemet ami a táblázatom lesz
-document.body.appendChild(table); //Hozzá appendelem a body-hoz
+    function createTableHeader(table) {//Elkezdem megirni a függvényt
+        const thead = document.createElement('thead'); // Létrehozok egy thead elemet
+        table.appendChild(thead); //Hozzá appendelem a táblázathoz
+    
+        const tr = document.createElement('tr'); //Létrehozok egy sor elemet
+        thead.appendChild(tr);//Hozzá appendelem a táblázathoz
+    
+        const uralkodo_nev = document.createElement('th');//Létrehozok egy th elemet
+        uralkodo_nev.innerHTML = "Uralkodo"; //Megadom az értékét
+        tr.appendChild(uralkodo_nev);//Hozzá appendelem a sorhoz
+    
+        const esemeny = document.createElement('th');
+        esemeny.innerHTML = "Esemeny";//Megadom az értékét
+        tr.appendChild(esemeny);//Hozzá appendelem a sorhoz
+    
+        const evszam = document.createElement('th');
+        evszam.innerHTML = "Evszam";//Megadom az értékét
+        tr.appendChild(evszam);//Hozzá appendelem a sorhoz
+    }
+    createTableHeader(table); // Meghívom a függvényt hogy létrehozza a táblázat fejlécét
 
-const thead = document.createElement('thead'); //Létrehozok egy thead elemet
-table.appendChild(thead);//Hozzá appendelem a táblázathoz
-
-const tr = document.createElement('tr');//Létrehozok egy sor elemet
-thead.appendChild(tr);//Hozzá appendelem a fej részhez
-
-const tbody = document.createElement('tbody');//Létrehozok egy tbody elemet
-table.appendChild(tbody)//Hozzá appendelem a table-höz
-
-const uralkodo_nev = document.createElement('th')//Létrehozok egy th elemet
-uralkodo_nev.innerHTML = "Uralkodo" //Megadom az értékét
-tr.appendChild(uralkodo_nev);//Hozzá appendelem a sorhoz
-
-const esemeny = document.createElement('th')//Létrehozok egy th elemet
-esemeny.innerHTML = "Esemeny"//Megadom az értékét
-tr.appendChild(esemeny)//Hozzá appendelem a sorhoz
-
-const evszam = document.createElement('th');//Létrehozok egy th elemet
-evszam.innerHTML = "Evszam"//Megadom az értékét
-tr.appendChild(evszam)//Hozzá appendelem a sorhoz
+    const tbody = document.createElement('tbody'); // Létrehozok egy tbody elemet
+    table.appendChild(tbody); // Hozzá appendelem a table-höz
 
 function render(){//Elkezdem megirni a render függvényt
     const tablebody = tbody;//Létrehozok egy tablebody-t aminek az értéke tbody
