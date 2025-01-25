6c548436ba707f8af312ac0dce87dbffefb9d6ab
Roli <rolandkortvelyesi12@gmail.com>
2025-01-25 16:26:53 +0100

    form generalas fuggveny bevezetese

--- main.js
+++ main.js
@@ -33,6 +33,33 @@ const array = [// A tömb létehozása
     }
 ]
 
+const form1 = [// A tömb létehozása
+    {// A tömb első elemét létrehozom
+        label: "Uralkodó neve:",//Értékadás
+        id: "uralkodo_nev",//Értékadás
+    },
+    {// A tömb második elemét létrehozom
+        label: "Első esemény:",//Értékadás
+        id: "esemeny1",//Értékadás
+    },
+    {// A tömb harmadik elemét létrehozom
+        label: "Első esemény évszáma:",//Értékadás
+        id: "evszam1"//Értékadás
+
+    },
+    {// A tömb negyedik elemét létrehozom
+        label: "Második esemény:",//Értékadás
+        id: "esemeny2",//Értékadás
+    },
+    {// A tömb ötödik elemét létrehozom
+        label: "Második esemény évszáma:",//Értékadás
+        id: "evszam2"//Értékadás
+
+    }   
+]
+
+generateForm() //Meghivom a függvényt
+
 const table = document.createElement('table'); //Létrehozok egy table elemet ami a táblázatom lesz
 document.body.appendChild(table); //Hozzá appendelem a body-hoz
 
@@ -129,6 +156,49 @@ thead.appendChild(tr);//Hozzá appendelem a fej részhez
 }
 
 const form = document.getElementById("form") //Lekérem a html form id-ját
+
+function generateForm(){  //Függvényt definiálunk
+    const form = document.createElement('form') //Létrehozom a formot
+    document.body.appendChild(form) //Hozzá appendelem a body-hoz
+    form.id = "form" //A form id-ja form
+    form.action = "#" //A form action-je #
+
+    for(let i = 0; i < form1.length; i++ ){  //A form összes elemén végig megyünk egy ciklussal
+        const div =  document.createElement('div') //Létrehozok egy div elemet
+        form.appendChild(div) //Hozzá appendelem a formhoz
+        
+        const label = document.createElement('label') //Létrehozok egy label elemet
+        div.appendChild(label) //Hozzá appendelem a div-hez
+
+        label.innerHTML = form1[i].label //A label innerHTML értéke a form1 objektumban eltárolt label lesz
+
+        const br = document.createElement('br') //Sortörést hozok létre
+        div.appendChild(br) //Hozzá appendelem a div-hez
+
+        const input = document.createElement('input') //Inputot hozok létre
+        div.appendChild(input)//Hozzá appendelem a div-hez
+        input.type = "text" //Input tipusa text
+        input.id = form1[i].id //Input id megegyezik az objektumban eltárolt id-val
+        input.name = form1[i].id //Input name megegyezik az objektumban eltárolt id-val
+
+        const br2 = document.createElement('br') //Sortörést hozok létre
+        div.appendChild(br2) //Hozzá appendelem a div-hez
+
+        const span = document.createElement('span') //Spant hozok létre
+        div.appendChild(span) //Hozzá appendelem a div-hez
+        span.className = "error" //A span classa az error lesz
+         
+        const br3 = document.createElement('br') //Sortörést hozok létre
+        div.appendChild(br3) //Hozzá appendelem a div-hez
+        
+    }
+
+    const button = document.createElement('button') //Létrehozok egy gombot
+    button.innerHTML = "Hozzáadás" //A gomb értéke a "Hozzáadás" szöveg lesz
+    form.appendChild(button) //Hozzá appendelem a formhoz a gombot
+}
+
+
 
 form.addEventListener('submit', function(e){//Eseménykezelőt adok a form-hoz
     e.preventDefault()//Megakadályozom hogy a böngésző alapártelmezetten lefusson
